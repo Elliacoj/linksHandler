fbbcfb5e73c7a7fd7d5c3c34cbb7790a579af6ec
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-11-04 12:02:15 +0100

    update button stat

--- view/homePage.view.php
+++ view/homePage.view.php
@@ -1,10 +1,8 @@
 <h1>Links handler</h1>
 <div id="menu"> <?php
     if(isset($_SESSION['id'])) { ?>
-    <div id="buttonAdd" title="Ajouter un lien"><i class="fas fa-plus-square"></i></div> <?php
-        if(isset($_SESSION['role']) && $_SESSION['role'] === "admin") { ?>
-    <div id="stat"><a href="index.php?controller=home&action=stat" title="Statistique"><i class="fas fa-chart-bar"></i></a></div> <?php
-        } ?>
+    <div id="buttonAdd" title="Ajouter un lien"><i class="fas fa-plus-square"></i></div>
+    <div id="stat"><a href="index.php?controller=home&action=stat" title="Statistique"><i class="fas fa-chart-bar"></i></a></div>
     <div id="buttonDisco"><a href="index.php?controller=user&action=logout" title="Déconnexion"><i class="fas fa-sign-out-alt"></i></a></div> <?php
     }?>
 </div>
